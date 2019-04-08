4422a7afb13b1074942b62bec7b1fb7670a30134
Ryan Mahoney <ryan@virtuecenter.com>
2019-04-08 11:34:06 -0400

    customer factory

--- src/common/factories.ts
+++ src/common/factories.ts
@@ -1,4 +1,4 @@
-import { CustomerActivity } from "@strong-platform/types";
+import { CustomerActivity, Customer } from "@strong-platform/types";
 
 const overrideDefaultValues = (defaultRecord: object, updatedRecord: object) =>
   Object.keys(defaultRecord).reduce(
@@ -9,28 +9,58 @@ const overrideDefaultValues = (defaultRecord: object, updatedRecord: object) =>
     {}
   );
 
-export const customerActivityFactory = (values: object): CustomerActivity => {
-  const defaultValues = {
-    id: null,
-    user_id: null,
-    session_id: "testsession",
-    activity_type: "PAGE_VIEW",
-    customer_id: null,
-    order_id: null,
-    subscription_id: null,
-    product_id: null,
-    product_variant_id: null,
-    status: "ok",
-    error_message: null,
-    path: null,
-    referring_url: null,
-    affiliate: null,
-    utm_source: null,
-    utm_medium: null,
-    utm_term: null,
-    utm_content: null,
-    utm_campaign: null,
-    context: null
-  };
-  return overrideDefaultValues(defaultValues, values) as CustomerActivity;
-};
+export const customer = (values: object): Customer =>
+  overrideDefaultValues(
+    {
+      id: null,
+      email: "test@test.com",
+      password: null,
+      first_name: "First",
+      last_name: "Last",
+      created: new Date(),
+      deleted: null,
+      created_by: null,
+      last_referring_url: null,
+      last_referring_url_date: null,
+      last_affiliate: null,
+      last_affiliate_date: null,
+      last_utm_source: null,
+      last_utm_source_date: null,
+      last_utm_medium: null,
+      last_utm_medium_date: null,
+      last_utm_term: null,
+      last_utm_term_date: null,
+      last_utm_content: null,
+      last_utm_content_date: null,
+      last_utm_campaign: null,
+      last_utm_campaign_date: null
+    },
+    values
+  ) as Customer;
+
+export const customerActivityFactory = (values: object): CustomerActivity =>
+  overrideDefaultValues(
+    {
+      id: null,
+      user_id: null,
+      session_id: "testsession",
+      activity_type: "PAGE_VIEW",
+      customer_id: null,
+      order_id: null,
+      subscription_id: null,
+      product_id: null,
+      product_variant_id: null,
+      status: "ok",
+      error_message: null,
+      path: null,
+      referring_url: null,
+      affiliate: null,
+      utm_source: null,
+      utm_medium: null,
+      utm_term: null,
+      utm_content: null,
+      utm_campaign: null,
+      context: null
+    },
+    values
+  ) as CustomerActivity;
